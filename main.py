c8a702921966f1a134d124144f38415a31295a88
FelipeBert <117488841+FelipeBert@users.noreply.github.com>
2022-12-04 14:17:32 -0300

    Verificação Senha

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-horaInicial = int(input("Digita hora de inicio do jogo: "))
-minutoInicial = int(input("Digita minuto de inicio do jogo: "))
-horaFinal = int(input("Digita hora de fim do jogo: "))
-minutoFinal = int(input("Digita minuto de fim do jogo: "))
+senha = 2202
+tentativa = int(input("Digite a senha: "))
 
-tempoHoras = horaFinal - horaInicial
-tempoMinutos = minutoFinal - minutoInicial
-
-if(tempoMinutos < 0):
-    tempoHoras -= 1
-    tempoMinutos = 60 + tempoMinutos
-
-print("O jogo durou {} horas e {} minutos".format(tempoHoras, tempoMinutos))
+while tentativa != 2202:
+    print("Senha Incorreta")
+    tentativa = int(input("Digite a senha: "))
+    
+print("Acesso Permitido!")
