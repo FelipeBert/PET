67db63a9b135d09982b2d6caa201073af8b1e1be
FelipeBert <117488841+FelipeBert@users.noreply.github.com>
2022-12-04 11:42:43 -0300

    Sistema de pontos da Copa
    Realização do exercício dos slides com base no sistema da Copa

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+vitorias = int(input("Indique o número de vitorias que a equipe teve: "))
+empates = int(input("Indique o número de empates que a equipe teve: "))
+derrotas = int(input("Indique o número de derrotas que a equipe teve: "))
+
+vitorias = vitorias * 3
+pontos = vitorias + empates
+print("Você teve {} pontos".format(pontos))
