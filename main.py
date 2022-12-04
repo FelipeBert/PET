4423985a36b59c082162eadbadf583ffea1807c5
FelipeBert <117488841+FelipeBert@users.noreply.github.com>
2022-12-04 14:44:29 -0300

    Fatorial

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-idadeSoma = 0
-idade = []
-idadeNegativa = 1
+def fatorial(num1):
+    for i in range(num1, 1, -1):
+        num1 *= i - 1
+    return num1
 
-while idadeNegativa != 0:
-    x = int(input("Digite sua idade: "))
-    if x < 0:
-        idadeNegativa = 0
-    else:
-        idade.append(x)
-        idadeSoma += x
-
-print("A média das idades foi: {}".format(idadeSoma / len(idade)))
+print(fatorial(10))
