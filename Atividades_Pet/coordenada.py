5b20dbb0b38b62b6f55071b46787d0ce4ea1e806
FelipeBert <117488841+FelipeBert@users.noreply.github.com>
2022-12-11 11:11:00 -0300

    Update coordenada.py

--- Atividades_Pet/coordenada.py
+++ Atividades_Pet/coordenada.py
@@ -1,12 +1,16 @@
-def pontos():
-    x1 = int(input("Digite o valor de x do primeiro ponto: "))
-    y1 = int(input("Digite o valor de y do primeiro ponto: "))
+x1 = int(input("Digite o valor de x do primeiro ponto: "))
+y1 = int(input("Digite o valor de y do primeiro ponto: "))
 
-    for i in range(5):
-        x_i = int(input("Digite o valor de x do ponto seguinte: "))
-        y_i = int(input("Digite o valor de y do ponto seguinte: "))
-        distancia = ( (x_i - x1)**2 + (y_i - y1)**2 ) ** (1/2)
-        print("A distância entre os pontos é de: ", distancia)
+def pontos(n_vezes):
+
+    x_i = int(input("Digite o valor de x do ponto seguinte: "))
+    y_i = int(input("Digite o valor de y do ponto seguinte: "))
+    distancia = ( (x_i - x1)**2 + (y_i - y1)**2 ) ** (1/2)
+    print("A distância entre os pontos é de: ", distancia)
+
+    if(n_vezes > 1):
+        pontos(n_vezes - 1)
 
 
-pontos()
+n = int(input("Digite o número de pontos a frente que quer calcular: "))
+pontos(n)
